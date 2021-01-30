aaddd49ba0cdec484f1d7fedda4e442ff25730a2
rudresh98 <ozarudresh98@gmail.com>
2021-01-31 02:35:54 +0530

    Final Output

--- JS/navabar.js
+++ JS/navabar.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll', function () {
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+function togglemenu() {
+    var menu = document.querySelector('.toggle');
+    menu.classList.toggle('active')
+    var menushow = document.querySelector('.menu');
+    menushow.classList.toggle('active')
+
+}
