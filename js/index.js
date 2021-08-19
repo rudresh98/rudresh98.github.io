310a65a40c4293440fb620dd9f94667dc3bcba60
rudreshoza <ozarudresh98@gmail.com>
2021-08-19 09:00:38 +0530

    project added

--- js/index.js
+++ js/index.js
@@ -1,43 +1,46 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 // particle js
 
 // dark mode added starts here
-let darkmode = localStorage.getItem('darkMode')
-console.debug("top", darkmode)
-var icon = document.getElementById('moon')
-var icon1 = document.getElementById('moon1')
+let darkmode = localStorage.getItem("darkMode");
+console.debug("top", darkmode);
+var icon = document.getElementById("moon");
+var icon1 = document.getElementById("moon1");
 enableDarkMode = () => {
-    document.body.classList.add("dark-theme")
-    localStorage.setItem('darkMode', "enable")
-    icon.classList = "fas fa-sun"
-    icon1.classList = icon.classList
-}
+  document.body.classList.add("dark-theme");
+  localStorage.setItem("darkMode", "enable");
+  icon.classList = "fas fa-sun";
+  icon1.classList = icon.classList;
+};
 disableDarkMode = () => {
-    document.body.classList.remove("dark-theme")
-    localStorage.removeItem('darkMode', null)
-    icon.classList = "fas fa-moon"
-    icon1.classList = icon.classList
-}
-if (darkmode === 'enable') {
-    enableDarkMode();
-}
-else {
-    disableDarkMode();
+  document.body.classList.remove("dark-theme");
+  localStorage.removeItem("darkMode", null);
+  icon.classList = "fas fa-moon";
+  icon1.classList = icon.classList;
+};
+if (darkmode === "enable") {
+  enableDarkMode();
+} else {
+  disableDarkMode();
 }
 toogleThemeClick = () => {
-    darkmode = localStorage.getItem('darkMode')
-    if (darkmode !== 'enable') {
-        enableDarkMode();
-    }
-    else {
-        disableDarkMode();
-    }
+  darkmode = localStorage.getItem("darkMode");
+  if (darkmode !== "enable") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+};
 
-}
+var load = document.getElementById("loading");
+loadfun = async () => {
+  load.style.display = "none";
+
+};
