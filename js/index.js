54f64c2c027b31e2d8b5d3898760a9941c2f3b74
Rudresh Oza <ozarudresh98@gmail.com>
2021-10-16 22:27:19 +0530

    form added

--- js/index.js
+++ js/index.js
@@ -6,8 +6,6 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-
-
 // dark mode added starts here
 let darkmode = localStorage.getItem("darkMode");
 console.debug("top", darkmode);
@@ -34,15 +32,37 @@ toogleThemeClick = () => {
   darkmode = localStorage.getItem("darkMode");
   if (darkmode !== "enable") {
     enableDarkMode();
-    console.debug("dark mode ENABLED😍")
+    console.debug("dark mode ENABLED😍");
   } else {
     disableDarkMode();
-    console.debug("dark mode DISABLED 😢")
+    console.debug("dark mode DISABLED 😢");
   }
 };
 
 var load = document.getElementById("loading");
 loadfun = async () => {
   load.style.display = "none";
-
+};
+const sendContactForm = () => {
+  const formData = {
+    form_subject: "Contact from Client (portfolio)",
+    form_name: document.getElementById("form-name").value,
+    form_id: document.getElementById("form-id").value,
+    to_name: "Rudresh Oza",
+    message: document.getElementById("message").value,
+  };
+  console.debug(formData);
+  emailjs
+    .send("service_qs0t96g", "template_ibljxpb", formData)
+    .then((res) => {
+      console.debug("SUCCESS", res);
+      alert("Your Data is Submitted successfully");
+    })
+    .catch((err) => {
+      console.debug("ERROR", err);
+      alert("Your data is not SUbmitted successfully");
+    });
+  document.getElementById("form-name").value = "";
+  document.getElementById("form-id").value = "";
+  document.getElementById("message").value = "";
 };
